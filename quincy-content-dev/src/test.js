a2fbf001a71ed02a8d17d7e2d75c972b25cb5c72
Lucas Ebert <lucascello@yahoo.com.br>
2022-05-28 01:50:02 +0200

    got rid of unnecessary code and logs

--- quincy-content-dev/src/test.js
+++ quincy-content-dev/src/test.js
@@ -1,62 +1,34 @@
 const fs = require("fs");
 
 describe("File loading test", () => {
-    function checkFiles(directoryPath) {
-        fs.readdir(directoryPath, { withFileTypes: true }, (err, content) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-
-            for (let i = 0; i < content.length; i++) {
-                if (content[i].isFile()) {
-                    fs.stat(
-                        `${directoryPath}/${content[i].name}`,
-                        "utf8",
-                        (err, metaData) => {
-                            if (err) {
-                                console.log(err);
-                                return;
-                            }
-                            // console.log(`${metaData.size}`);
-                            console.log(`${directoryPath}/${content[i].name}`);
-                        }
-                    );
-                } else {
-                    checkFiles(`${directoryPath}/${content[i].name}`);
-                }
-            }
-        });
-    }
-
-    //checkFiles(`${__dirname}/content`);
-
-    function mapSizes(directoryPath) {
+    function mapFoldersAndFiles(directoryPath) {
         const dirContent = fs.readdirSync(directoryPath, {
             withFileTypes: true,
         });
 
         const objFromMap = {};
 
-        dirContent.forEach(function (item) {
+        dirContent.forEach((item) => {
             const path = directoryPath + "/" + item.name;
             if (item.isFile()) {
                 const metaData = fs.statSync(path);
 
                 objFromMap[item.name] = metaData.size;
             } else {
-                let nextObj = mapSizes(path);
+                let nextObj = mapFoldersAndFiles(path);
                 objFromMap[item.name] = nextObj;
             }
         });
         return objFromMap;
     }
 
-    // console.log(JSON.stringify(mapSizes(`${__dirname}/content`), null, 2));
+    // console.log(
+    //     JSON.stringify(mapFoldersAndFiles(`${__dirname}/content`), null, 2)
+    // );
 
     test("Compare the flyer.scr and the yml file (4519_note_values.yml)", () => {
         const filePath = JSON.stringify(
-            mapSizes(`${__dirname}/content/beginner1/lesson2`)
+            mapFoldersAndFiles(`${__dirname}/content/beginner1/lesson2`)
         );
 
         // console.log("**********", filePath);
@@ -65,7 +37,7 @@ describe("File loading test", () => {
 
     test("Compare the expectation.scr and the yml file (4519_note_values.yml)", () => {
         const filePath = JSON.stringify(
-            mapSizes(`${__dirname}/content/beginner1/lesson2`)
+            mapFoldersAndFiles(`${__dirname}/content/beginner1/lesson2`)
         );
 
         expect(filePath).toContain("D_D_1.csv");
@@ -73,7 +45,7 @@ describe("File loading test", () => {
 
     test("Compare the startVoiceOver.src and the yml file (4519_note_values.yml)", () => {
         const filePath = JSON.stringify(
-            mapSizes(`${__dirname}/content/beginner1/lesson2`)
+            mapFoldersAndFiles(`${__dirname}/content/beginner1/lesson2`)
         );
 
         expect(filePath).toContain("long_notes.aifc");
@@ -81,7 +53,7 @@ describe("File loading test", () => {
 
     test("Compare the listenAudioCue.src and the yml file (4519_note_values.yml)", () => {
         const filePath = JSON.stringify(
-            mapSizes(`${__dirname}/content/beginner1/lesson2`)
+            mapFoldersAndFiles(`${__dirname}/content/beginner1/lesson2`)
         );
 
         expect(filePath).toContain("D_D_2.aifc");
@@ -89,7 +61,7 @@ describe("File loading test", () => {
 
     test("Compare the handvideo.src and the yml file(4519_note_values.yml)", () => {
         const filePath = JSON.stringify(
-            mapSizes(`${__dirname}/content/beginner1/lesson2`)
+            mapFoldersAndFiles(`${__dirname}/content/beginner1/lesson2`)
         );
 
         expect(filePath).toContain("dd.mp4");
