3e391c481f9643955e5462fc036f31186d8c6513
Lucas Ebert <lucascello@yahoo.com.br>
2022-05-28 01:05:52 +0200

    cleaned up the toContain statements, so the tests are clearer

--- quincy-content-dev/src/index.test.js
+++ quincy-content-dev/src/index.test.js
@@ -55,43 +55,43 @@ describe("File loading test", () => {
     // console.log(JSON.stringify(mapSizes(`${__dirname}/content`), null, 2));
 
     test("Compare the flyer.scr and the yml file (4519_note_values.yml)", () => {
-        const filePath = JSON.stringify(mapSizes(`${__dirname}/content`));
+        const filePath = JSON.stringify(
+            mapSizes(`${__dirname}/content/beginner1/lesson2`)
+        );
 
         console.log("**********", filePath);
-        expect(filePath).toContain(
-            '"img":{"dd_1.svg":17276,"de":{"DDffDDff.svg":27270,"D_D_1.svg":12409}'
-        );
+        expect(filePath).toContain("D_D_1.svg");
     });
 
     test("Compare the expectation.scr and the yml file (4519_note_values.yml)", () => {
-        const filePath = JSON.stringify(mapSizes(`${__dirname}/content`));
-
-        expect(filePath).toContain(
-            '"expect":{"DDffDDff.csv":126,"ddFddF_1.csv":100,"dd_1.csv":74, "D_D_1.csv"}'
+        const filePath = JSON.stringify(
+            mapSizes(`${__dirname}/content/beginner1/lesson2`)
         );
+
+        expect(filePath).toContain("D_D_1.csv");
     });
 
     test("Compare the startVoiceOver.src and the yml file (4519_note_values.yml)", () => {
-        const filePath = JSON.stringify(mapSizes(`${__dirname}/content`));
-
-        expect(filePath).toContain(
-            '"voice":{"de":{"long_notes.aifc":127108,"short_long_notes_combined.aifc":150092,"short_notes.aifc":79304}'
+        const filePath = JSON.stringify(
+            mapSizes(`${__dirname}/content/beginner1/lesson2`)
         );
+
+        expect(filePath).toContain("long_notes.aifc");
     });
 
     test("Compare the listenAudioCue.src and the yml file (4519_note_values.yml)", () => {
-        const filePath = JSON.stringify(mapSizes(`${__dirname}/content`));
-
-        expect(filePath).toContain(
-            '"audio":{"D_D_2.aifc":160292,"ddFddF_1.aifc":160292,"dd_1.aifc":160292}'
+        const filePath = JSON.stringify(
+            mapSizes(`${__dirname}/content/beginner1/lesson2`)
         );
+
+        expect(filePath).toContain("D_D_2.aifc");
     });
 
     test("Compare the handvideo.src and the yml file(4519_note_values.yml)", () => {
-        const filePath = JSON.stringify(mapSizes(`${__dirname}/content`));
-
-        expect(filePath).toContain(
-            '"lesson2":{"4519_note_values":{"4519_note_values.yml":1682,"audio":{"D_D_1.aifc":160292,"ddFddF_1.aifc":160292,"dd_1.aifc":160292},"expect":{"DDffDDff.csv":126,"ddFddF_1.csv":100,"dd_1.csv":74},"img":{"dd_1.svg":17276,"de":{"DDffDDff.svg":27270,"D_D_1.svg":12409},"en":{"DDffDDff.svg":27270,"D_D_1.svg":12409}},"index.yml":45,"video":{"dd.mp4"'
+        const filePath = JSON.stringify(
+            mapSizes(`${__dirname}/content/beginner1/lesson2`)
         );
+
+        expect(filePath).toContain("dd.mp4");
     });
 });
